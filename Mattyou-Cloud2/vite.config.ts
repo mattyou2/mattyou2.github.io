ac63d286e4e64362218f3634e8946f630b5d6387
mattyou2 <mattyougaming@gmail.com>
2026-05-08 19:56:45 +0200

    Update vite.config.ts

--- Mattyou-Cloud2/vite.config.ts
+++ Mattyou-Cloud2/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Mattyou-Cloud2/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
