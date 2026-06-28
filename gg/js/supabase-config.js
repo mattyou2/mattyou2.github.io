29fb16e5767a759448f2d0ca4772489dcc6c65d0
mattyou2 <mattyougaming@gmail.com>
2026-06-28 14:01:57 +0200

    Update supabase-config.js

--- gg/js/supabase-config.js
+++ gg/js/supabase-config.js
@@ -5,8 +5,8 @@
 // Ga naar: https://supabase.com/dashboard/project/_/settings/api
 // =============================================
 
-const SUPABASE_URL = 'JOUW_SUPABASE_URL_HIER';
-const SUPABASE_ANON_KEY = 'JOUW_SUPABASE_ANON_KEY_HIER';
+const SUPABASE_URL = 'https://hydnkvuhuoyqeusjlwyr.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5ZG5rdnVodW95cWV1c2psd3lyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2MzU1MzgsImV4cCI6MjA5ODIxMTUzOH0.eVc879oP59tw4NbIa3r-xcrJNMmH57Cw7toZmehOIpA';
 
 // Initialiseer Supabase client
 const { createClient } = supabase;
